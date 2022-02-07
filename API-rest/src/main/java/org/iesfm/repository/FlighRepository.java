15593957ba21fd4a01eeab0bf8feaac175192ad9
Ruben <martinillo114@gmail.com>
2022-02-08 00:33:22 +0100

    hello world

--- API-rest/src/main/java/org/iesfm/repository/FlighRepository.java
+++ API-rest/src/main/java/org/iesfm/repository/FlighRepository.java
@@ -1,7 +1,6 @@
 package org.iesfm.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.iesfm.pojos.Flight;
 
-public interface FlighRepository extends MongoRepository<> {
+public interface FlighRepository {
 }
